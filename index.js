c3ab428293a85f6e793751e947981fdc8ff59fbc
salmanfarisk <salmanfarisk98@gmail.com>
2024-03-26 11:45:42 +0530

    Fake commit on 2024-03-26 11:45:42

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 
 function generateRandomDate() {
   // Generate a random date between 2010 and 2025
-  const start = new Date(2022, 0, 1); 
+  const start = new Date(2024, 0, 1); 
   const end = new Date(2025, 0, 1);
   return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
 }
@@ -37,4 +37,4 @@ function createFakeCommits(num) {
 }
 
 // Run the function to create 10 fake commits
-createFakeCommits(50);
+createFakeCommits(30);
